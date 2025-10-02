04f24b9263995b4c18807eff2af8fafd86149c32
LEE JUNG WOO <54746349+prayslaks@users.noreply.github.com>
2025-10-02 13:22:19 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int diff = 2541 - 1998;
+	int budishYear;
+	cin >> budishYear;
+	cout << budishYear - diff << endl;
+}
